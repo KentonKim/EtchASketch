1bbeb59a79d6b6c1112df49d7cf103d4aaf056c2
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-02-24 16:50:04 +0000

    rotate to isometric view add to animation when hover

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const gridDiv = document.querySelector('.grid')
 const style = getComputedStyle(gridDiv, 'width')
 const gridWidth = 960
-const dimension = 16
+const dimension = 8
 const boxWidth = (100/dimension).toString() + "%"
 
 gridDiv.style.width = (gridWidth).toString() + 'px'
@@ -12,22 +12,33 @@ for (let i = 0; i < dimension**2; i++) {
     box.classList.add('box')
     box.style.width = boxWidth 
     box.style.height = boxWidth
+    box.addEventListener('mouseenter', hoverEffect)
 
     let innerbox = document.createElement('div')
     innerbox.classList.add('innerbox')
-    innerbox.addEventListener('mouseenter', hoverEffect)
 
     box.appendChild(innerbox)
     gridDiv.appendChild(box)
 }
 
+const BoxArray = document.querySelectorAll('.box')
+const innerBoxArray = document.querySelectorAll('.innerbox')
 
 function hoverEffect(e) {
-    console.log(e.target)
-    e.target.animate([
+    e.target.children[0].animate([
         {offset: 0.5, backgroundColor: 'skyblue'},
+        {offset: 0.5, border: '1px solid blue'},
+        {offset: 0.5, transform: 'translateX(-5px) translateY(-5px)'},
         {offset: 0.99, backgroundColor: 'rosybrown'},
+        {offset: 0.99, transform: 'translateY(0px)'},
     ], {
         duration: 2000
     }) 
+    e.target.animate([
+        {offset: 0.5, backgroundColor: 'skyblue'},
+        {offset: 0.99, backgroundColor: 'burlywood'},
+    ], {
+        duration: 2000
+    }) 
+
 }
