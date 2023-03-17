2ba19ae008ab725496daf117586b7b88e75db96a
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2023-03-17 14:28:11 +0000

    add erase button separate animation to class add fill color when mousedown and hover

--- app.js
+++ app.js
@@ -1,96 +1,119 @@
-const grid = document.querySelector('.grid')
-const butt = document.querySelector('.button')
-const GRIDWIDTH = 960
-const DIMENSION = 10
-const BOXWIDTH = (100/DIMENSION).toString() + "%"
+const grid = document.querySelector('.grid');
+const button = document.querySelector('#bttn__updategrid');
+const GRIDWIDTH = 960;
+const DIMENSION = 10;
+const BOXWIDTH = (100/DIMENSION).toString() + "%";
+const colorArray = ['#CC99C9', '#9EC1CF', '#9EE09E', '#FDFD97', '#FEB144','#bae1ff', '#FF6663'];
+let brushColor = 'random'
+let effectColor = 'white'
+let mouseDown = 0;
 
-grid.style.width = (GRIDWIDTH).toString() + 'px'
-grid.style.height = (GRIDWIDTH).toString() + 'px'
-grid.style.gridTemplateColumns = `repeat(${DIMENSION}, 1fr)`
-grid.style.gridTemplateRows = `repeat(${DIMENSION}, 1fr)`
+grid.style.width = (GRIDWIDTH).toString() + 'px';
+grid.style.height = (GRIDWIDTH).toString() + 'px';
+grid.style.gridTemplateColumns = `repeat(${DIMENSION}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${DIMENSION}, 1fr)`;
 
-for (let i = 0; i < DIMENSION**2; i++) {
-    let box = document.createElement('div')
-    box.classList.add('box')
-
-    let innerbox = document.createElement('div')
-    innerbox.classList.add('innerbox')
-    innerbox.addEventListener('mouseenter', hoverEvent)
-    innerbox.addEventListener('click', rippleEvent)
-  
-    box.appendChild(innerbox)
-    grid.appendChild(box)
+document.body.onmousedown = function() { 
+  mouseDown = 1;
+}
+document.body.onmouseleave= function() { 
+  mouseDown = 0;
+}
+document.body.onmouseup = function() {
+  mouseDown = 0;
 }
 
-const BoxArray = document.querySelectorAll('.box')
-const innerBoxArray = document.querySelectorAll('.innerbox')
+for (let i = 0; i < DIMENSION**2; i++) {
+    let box = document.createElement('div');
+    box.classList.add('box');
+
+    let innerbox = document.createElement('div');
+    innerbox.classList.add('innerbox');
+    innerbox.addEventListener('click', rippleEvent);
+    innerbox.addEventListener('mousedown', fillColorEvent);
+    innerbox.addEventListener('mouseenter', hoverEvent);
+    innerbox.addEventListener('mouseenter', fillColorEvent);
+  
+    box.appendChild(innerbox);
+    grid.appendChild(box);
+}
+
+const BoxArray = document.querySelectorAll('.box');
+const innerBoxArray = document.querySelectorAll('.innerbox');
 
 function hoverEvent(e) {
-    hoverEffect(e.target)
+    hoverEffect(e.target);
+}
+
+function fillColorEvent(e) {
+    if (e.type == "mousedown") {
+        fillColorEffect(e.target,brushColor);
+    }
+    else if (e.type == "mouseenter" && mouseDown) {
+        fillColorEffect(e.target,brushColor);
+    }
 }
 
 function hoverEffect(targ) {
-    let HOVER_DURATION = 200
+    let HOVER_DURATION = 150;
     if (!targ.classList.contains('hovering')) {
-        targ.classList.add('hovering')
+        targ.classList.add('hovering');
         targ.animate([
-            {offset: 0.5, backgroundColor: 'white'},
-            // {offset: 0.5, transform: 'translateX(-3px) translateY(-3px)'},
-            // {offset: 0.99, transform: 'translateY(0px)'},
+            {offset: 0.5, backgroundColor: effectColor},
         ], {
-            duration: HOVER_DURATION 
-        })
+            duration: HOVER_DURATION
+        });
         setTimeout(() => {
-            targ.classList.remove('hovering')
+            targ.classList.remove('hovering');
         }, HOVER_DURATION);
     }
 }
 
 function rippleEvent(e) {
-    let RIPPlE_DURATION = 80
+    let RIPPlE_DURATION = 80;
     let startNum = Array.prototype.indexOf.call(innerBoxArray, e.target);
-    corner(startNum,'n')
-    corner(startNum,'e')
-    corner(startNum,'s')
-    corner(startNum,'w')
+    corner(startNum,'n');
+    corner(startNum,'e');
+    corner(startNum,'s');
+    corner(startNum,'w');
 
     function corner(number,direction){
         if (number >=0 && number < DIMENSION**2) {
-            hoverEffect(innerBoxArray[number])
+            hoverEffect(innerBoxArray[number]);
         }
         if (direction == 'n'){
-            straight(number,'nw')
-            straight(number,'ne')
+            straight(number,'nw');
+            straight(number,'ne');
             if (number % DIMENSION != 0 && number >= 0){
                 setTimeout(() => {
-                    corner(number-DIMENSION-1,'n')
+                    corner(number-DIMENSION-1,'n');
                 }, RIPPlE_DURATION);
             }
         }
         else if (direction == 'e'){
-            straight(number,'ne')
-            straight(number,'se')
+            straight(number,'ne');
+            straight(number,'se');
             if ((number+1) % DIMENSION != 0 && number >= 0){
                 setTimeout(() => {
-                corner(number-DIMENSION+1,'e')
+                corner(number-DIMENSION+1,'e');
                 }, RIPPlE_DURATION);
             }
         }
         else if (direction == 's'){
-            straight(number,'se')
-            straight(number,'sw')
+            straight(number,'se');
+            straight(number,'sw');
             if ((number+1) % DIMENSION != 0 && number < DIMENSION**2){
                 setTimeout(() => {
-                corner(number+DIMENSION+1,'s')
+                corner(number+DIMENSION+1,'s');
                 }, RIPPlE_DURATION);
             }
         }
         else if (direction == 'w'){
-            straight(number,'nw')
-            straight(number,'sw')
+            straight(number,'nw');
+            straight(number,'sw');
             if (number % DIMENSION != 0 && number < DIMENSION**2){
                 setTimeout(() => {
-                    corner(number+DIMENSION-1,'w')
+                    corner(number+DIMENSION-1,'w');
                 }, RIPPlE_DURATION);
             }
         }
@@ -98,36 +121,59 @@ function rippleEvent(e) {
 
     function straight(number,direction){
         if (number >= 0 && number < DIMENSION**2){
-            hoverEffect(innerBoxArray[number])
+            hoverEffect(innerBoxArray[number]);
         }
         if (direction == 'nw' && number % DIMENSION !=0 && number-1 >=0) {
             setTimeout(() => {
-                straight(number-1,'nw')
+                straight(number-1,'nw');
             }, RIPPlE_DURATION);
         }
         else if (direction == 'ne' && number >=0) {
             setTimeout(() => {
-                straight(number-DIMENSION,'ne')
+                straight(number-DIMENSION,'ne');
             }, RIPPlE_DURATION);
         }
         else if (direction == 'sw' && number < DIMENSION**2) {
             setTimeout(() => {
-                straight(number+DIMENSION,'sw')
+                straight(number+DIMENSION,'sw');
             }, RIPPlE_DURATION);
         }
         else if (direction == 'se' && (number+1) % DIMENSION !=0) {
             setTimeout(() => {
-                straight(number+1,'se')
+                straight(number+1,'se');
             }, RIPPlE_DURATION);
         }
    }
 }
 
-butt.addEventListener('click', testfxn)
-function testfxn() {
-    for (i = 0; i < 400; i++) {
+function fillColorEffect(targ,color) {
+    if (color == 'black') {
+        targ.style.backgroundColor = "black";
+    }
+    else if (color == 'white') {
+        targ.style.backgroundColor = "white";
+    }
+    else if (color == 'random') {
+        /*
+        let randomColor = Math.floor(Math.random() * 7);
+        targ.style.backgroundColor = colorArray[randomColor];
+        */
+        targ.classList.add('animate');
+    }
+}
+
+button.addEventListener('click',eraseGrid)
+
+function eraseGrid() {
+    let eraseDelayMillisecond = 10
+    for (let i = 0; i < DIMENSION**2; i++) {
+        eraseDelayMillisecond += 10 
         setTimeout(() => {
-            corner(i,'n') 
-        }, 1000);
+            console.log(eraseDelayMillisecond)
+            innerBoxArray[i].style.backgroundColor = 'transparent';
+            if (innerBoxArray[i].classList.contains('animate')){
+                innerBoxArray[i].classList.remove('animate')
+            }
+        }, eraseDelayMillisecond);
     }
 }
